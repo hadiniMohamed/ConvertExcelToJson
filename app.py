185f0e251103a0d1ff62d891d46344df69b1cee9
hadini <mohamed.hadini@interaktiondigital.com>
2024-09-23 09:22:03 +0100

    refactor : clean code

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def upload_file():
     if file:
         # Read the Excel file
         df = pd.read_excel(file)
-        columns = df.columns.tolist()  # Get the columns of the file
-        return jsonify(columns=columns)  # Return as a JSON object
+        columns = df.columns.tolist() 
+        return jsonify(columns=columns)
 
 @app.route('/convert', methods=['POST'])
 def convert_to_json():
@@ -31,26 +31,19 @@ def convert_to_json():
     selected_columns = request.form.getlist('columns')
     json_filename = request.form.get('json_filename')
 
-    # Read the uploaded Excel file
     df = pd.read_excel(file)
-
-    # Select the specified columns
     df = df[selected_columns]
 
-    # Convert Timestamp columns to string
     for column in df.columns:
         if pd.api.types.is_datetime64_any_dtype(df[column]):
             df[column] = df[column].apply(lambda x: x.strftime('%Y-%m-%d') if pd.notna(x) else None)
 
-    # Convert DataFrame to JSON
     data = df.to_dict(orient='records')
 
-    # Create the JSON file
     json_filepath = f"{json_filename}.json"
     with open(json_filepath, 'w') as json_file:
         json.dump(data, json_file, indent=2)
 
-    # Return the JSON file for download
     return send_file(json_filepath, as_attachment=True)
 
 if __name__ == '__main__':
